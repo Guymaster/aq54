bb92bae2c6080a7b1f4fed0cdc2fb23af700bc90
Guymaster <guymaster25@gmail.com>
2024-02-20 18:52:23 +0000

    Setting up login page

--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -1,14 +1,42 @@
+import { useState } from "react";
+import "./../../fonts/ubuntu.css";
 import "./LoginPage.css";
 
 function LoginPage() {
-    return (
-      <main className="LoginPage">
-        <form>
-            <div>Enter you credentials</div>
-            <label></label>
-        </form>
-      </main>
-    );
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <main className="LoginPage">
+      <form>
+          <div className="formTitle">Enter you credentials</div>
+          <label>Username</label>
+          <input 
+            type="text"
+            value={username} 
+            onChange={handleUsernameChange}
+          />
+          <label>Password</label>
+          <input 
+            type="password" 
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <input className="submitBTN" type="submit" onClick={handleSubmit} value={isLoading? "..." : "Connexion"} />
+      </form>
+    </main>
+  );
 }
   
 export default LoginPage;
