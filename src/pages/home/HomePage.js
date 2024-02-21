ca6e034e5f167ad537341cc654e6b0467f6b5239
Guymaster <guymaster25@gmail.com>
2024-02-21 13:00:50 +0000

    Setting up home page

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,9 +1,13 @@
-//
+import "./HomePage.css";
 
 function HomePage() {
     return (
       <main className="HomePage">
-        HOMEPAGE
+        <div className="title">Projet AQ54</div>
+        <div className="description">
+          Etudions la qualité de l'air à Abidjan en mesurant la quantité de particules fines grâces à des capteurs Airqino de 300m de rayon chacun.
+        </div>
+        <div className="goBTN">Visualiser les données</div>
       </main>
     );
 }
